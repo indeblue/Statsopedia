bc6448ab49bf348de8c353b28e0a50c19aeaa8ff
Akshay Soni <46927156+indeblue@users.noreply.github.com>
2021-04-26 10:31:05 +0530

    Update hk.js

--- hk.js
+++ hk.js
@@ -7,7 +7,7 @@ var myChart = new Chart(ctx, {
 
     data: {
 
-        labels: ["17/4", "18/4", "19/4", "20/4", "21/4", "22/4", "23/4", "24/4"],
+        labels: ["17/4", "18/4", "19/4", "20/4", "21/4", "22/4", "23/4", "24/4","25/4"],
 
         datasets: [
 
@@ -15,11 +15,11 @@ var myChart = new Chart(ctx, {
 
                 label: "New Covid-19 Cases",
 
-                data: [260778, 275306, 256947, 294290, 315802, 332503, 345147, 349313],
+                data: [260778, 275306, 256947, 294290, 315802, 332503, 345147, 349313, 354531],
 
 
                 backgroundColor: [
-                    "rgb(255, 166, 1)", "rgb(255, 166, 1)", "rgb(255, 166, 1)", "rgb(255, 166, 1)", "rgb(255, 166, 1)", "rgb(255, 166, 1)", "rgb(255, 166, 1)", "rgb(255, 166, 1)"
+                    "rgb(255, 166, 1)", "rgb(255, 166, 1)", "rgb(255, 166, 1)", "rgb(255, 166, 1)","rgb(255, 166, 1)", "rgb(255, 166, 1)", "rgb(255, 166, 1)", "rgb(255, 166, 1)", "rgb(255, 166, 1)"
                 ],
                 //           {
                 //data: [123, 233, 1233, 133, 19345],
@@ -27,8 +27,9 @@ var myChart = new Chart(ctx, {
             },
             {
                 label: "New Recoveries",
-                data: [138205, 143754, 154372, 166643, 179543, 192200, 220513, 215962],
+                data: [138205, 143754, 154372, 166643, 179543, 192200, 220513, 215962,218559],
                 backgroundColor: ["green", "green",
+                    "green",
                     "green",
                     "green",
                     "green",
@@ -49,3 +50,18 @@ var myChart = new Chart(ctx, {
     },
 
 });
+
+const counter = document.querySelectorAll('.count');
+counter.forEach((count) => {
+    count.innerHTML = 0;
+    const updatecounter = () => {
+        const target = +count.getAttribute('data-target');
+        const startingcount = Number(count.innerHTML);
+        const incr = target / 10;
+        if (startingcount < target) {
+            count.innerHTML = `${Math.round(startingcount + incr)}`;
+            setTimeout(updatecounter, 200)
+        } else { count.innerHTML = target; }
+    }
+    updatecounter();
+})
